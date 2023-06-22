d73bb431e45e47a413f2ca75ecd212da51741a9f
André Antunes <andrem.ferraz@hotmail.com>
2023-06-22 03:36:21 -0300

    migration fix

--- prisma/migrations/20230622063541_create_users_and_cats/migration.sql
+++ prisma/migrations/20230622063541_create_users_and_cats/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "age" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
@@ -18,7 +18,14 @@ CREATE TABLE "Cat" (
     "race" TEXT NOT NULL,
     "castrated" TEXT NOT NULL,
     "vaccines" TEXT NOT NULL,
+    "ownerId" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Cat_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+
+-- AddForeignKey
+ALTER TABLE "Cat" ADD CONSTRAINT "Cat_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
